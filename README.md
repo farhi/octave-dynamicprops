03959a8902cc9ff48e5ffcc6bc9fb11c09dc086e
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2019-10-30 13:47:59 +0100

    more doc and few bug fixes.

--- README.md
+++ README.md
@@ -1,28 +1,45 @@
 # octave-dynamicprops
 An Octave class similar to the Matlab dynamicprops
 
+**dynamicprops** is a class derived from the ```handle``` class. Subclass ```dynamicprops``` to define classes that support dynamic properties.
+Dynamic properties are associated with a specific object of the class, but are not part of the class definition. Use dynamic properties to attach temporary data to objects. 
+
 This class allows to add dynamically properties, just as the dynamicprops one works:
 - https://fr.mathworks.com/help/matlab/ref/dynamicprops-class.html
 
 Currently, it includes:
-- the 'propertyAdded' event
-- the addprop method
-- a subsref method (called for e.g. obj.property)
-- a subsasgn method (called for e.g. obj.property = value)
+- the ```PropertyAdded``` event (but not supported by Octave 4.2)
+- the ```addprop``` method
+- a ```subsref``` method (called with syntax e.g. ```obj.property```)
+- a ```subsasgn``` method (called with syntax e.g. ```obj.property = value```)
 
 Usage
 =====
-
-The class derives from handle, so that defining your class as:
+The class derives from ```handle```, so that defining your class as:
 ```matlab
 classdef blah < dynamicprops
   ...
 end
 ```
 also sets it as a 'handle', that is a reference (pointer) object, with the attached methods and properties.
+See https://octave.org/doc/v4.2.2/Value-Classes-vs_002e-Handle-Classes.html#Value-Classes-vs_002e-Handle-Classes
 
+
+Then use:
+```matlab
+obj = blah;
+addprop(obj, 'field')
+obj.field = 42;
+```
+
+NOTE
+====
 If you come to redefine 'subsref' and 'subsasgn' methods, you will need to add a line such as:
 ```
 S = subs_added(a,S);
 ```
 to allow the substructure S to search for added properties.
+
+Credits
+=======
+(c) E. Farhi / Synchrotron Soleil (2019), GPL 2.
--- dynamicprops.m
+++ dynamicprops.m
@@ -1,4 +1,45 @@
 classdef dynamicprops < handle
+% DYNAMICPROPS superclass for classes that support dynamic properties
+%
+% dynamicprops is a class derived from the handle class. Subclass dynamicprops 
+% to define classes that support dynamic properties.
+%
+% Dynamic properties are associated with a specific object of the class, but are
+% not part of the class definition. Use dynamic properties to attach temporary data to objects. 
+% 
+% See:
+%  - https://fr.mathworks.com/help/matlab/ref/dynamicprops-class.html
+%
+%  Currently, it includes:
+%  - the 'propertyAdded' event
+%  - the 'addprop' method
+%  - a 'subsref' method (called with syntax e.g. 'obj.property')
+%  - a 'subsasgn' method (called with syntax e.g. 'obj.property = value')
+%
+% Usage
+% =====
+% The class derives from 'handle', so that defining your class as:
+%
+%   classdef blah < dynamicprops
+%   ...
+%   end
+%  
+% also sets it as a 'handle', that is a reference (pointer) object, with the 
+% associated methods and properties.
+% See https://octave.org/doc/v4.2.2/Value-Classes-vs_002e-Handle-Classes.html#Value-Classes-vs_002e-Handle-Classes
+%
+% Then use:
+%   obj = blah;
+%   addprop(obj, 'field')
+%   obj.field = 42;
+%  
+% NOTE
+% ====
+% If you come to redefine 'subsref' and 'subsasgn' methods, you will need to add a line such as:
+% 
+%   S = subs_added(a,S);
+%  
+% to allow the substructure S to search for added properties.
 
   properties
     dynamicprops_added = []; % Properties dynamically added
@@ -16,12 +57,16 @@ classdef dynamicprops < handle
       %  the objects of H.  There is no effect on the class of H.
 
       %  See also dynamicprops, handle
-      if ~ischar(prop) || isempty(prop) || isempty(h)
+      if nargin < 2 || ~ischar(prop) || isempty(prop) || isempty(h)
         error([ mfilename ': addprop: Parameter must be a string.' ]); 
+      end
+      prop = prop(:)';
+      if ~isvarname(prop)
+        error([ mfilename ': addprop: Parameter must be a valid property name.' ]); 
       end
       for index=1:numel(h)
         this = h(index);
-        this.dynamicprops_added.(prop(:)')=[];
+        this.dynamicprops_added.(prop)=[];
         h(index) = this;
         try; notify(this, 'PropertyAdded'); end
       end
@@ -55,9 +100,12 @@ classdef dynamicprops < handle
   
     function S = subs_added(a,S)
       % SUBS_ADDED Take into account the dynamic properties
+      if isempty(S), return; end
+      if isempty(a.dynamicprops_added), return; end
       if ischar(S), S=struct('type','.','subs', S); end
       % we search for fields that match any of the dynamicprops_added elements
       % and add a new level above with it
+      
       f     = fieldnames(a.dynamicprops_added);
       index = find(strcmp(S(1).subs, f));
       if strcmp(S(1).type, '.') && ~isempty(index)
